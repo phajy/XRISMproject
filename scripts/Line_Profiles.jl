ac289404fed3d43f7439578a3b6339e5ef2d6a00
phajy <andy.young@bristol.ac.uk>
2024-11-05 16:36:41 +0000

    Profile for narrow component of line

--- scripts/Line_Profiles.jl
+++ scripts/Line_Profiles.jl
@@ -1,19 +1,20 @@
 using Gradus
 using Plots
 
-d = ThinDisc(0.0, 400.0)
-x = SVector(0.0, 1000.0, deg2rad(40), 0.0)
+d = ThinDisc(0.0, 7000.0)
+x = SVector(0.0, 100_000.0, deg2rad(12), 0.0)
 m = KerrMetric(1.0, 0.998)
 
 # maximal integration radius
-maxrₑ = 50.0
+minrₑ = 4000.0
+maxrₑ = 7000.0
 
 # emissivity function
 ε(r) = r^(-3)
 
 # g grid to do flux integration over
-gs = range(0.0, 1.2, 500)
-_, flux = lineprofile(gs, ε, m, x, d, maxrₑ = maxrₑ, verbose = true)
+gs = range(0.8, 1.2, 500)
+_, flux = lineprofile(gs, ε, m, x, d, minrₑ = minrₑ, maxrₑ = maxrₑ, verbose = true)
 
 # plot flux as a function of energy
 plot(gs, flux, legend=false, xlabel = "Energy", ylabel = "Flux")
