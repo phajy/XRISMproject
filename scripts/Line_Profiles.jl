72e220cdf67047a4382a0f129684b0677585b8d8
phajy <andy.young@bristol.ac.uk>
2024-11-12 16:32:21 +0000

    feat: combined line profiles

--- scripts/Line_Profiles.jl
+++ scripts/Line_Profiles.jl
@@ -25,26 +25,29 @@ K_narr = 1.5
 # emissivity function
 ε(r) = r^(-3)
 
-# g grid to do flux integration for broad line
-gs_broad = range(0.7, 1.1, 500)
-_, flux_broad = lineprofile(gs_broad, ε, m, x_broad, d, minrₑ = minrₑ_broad, maxrₑ = maxrₑ_broad, verbose = true)
+# g grid
+gs = range(0.7, 1.1, 500)
+
+# do flux integration for broad line
+_, flux_broad = lineprofile(gs, ε, m, x_broad, d, minrₑ = minrₑ_broad, maxrₑ = maxrₑ_broad, verbose = true)
 flux_broad = K_broad * flux_broad
 
-# g grid to do flux integration for intermediate line
-gs_int = range(0.7, 1.1, 500)
-_, flux_int = lineprofile(gs_int, ε, m, x_int, d, minrₑ = minrₑ_int, maxrₑ = maxrₑ_int, verbose = true)
+# do flux integration for intermediate line
+_, flux_int = lineprofile(gs, ε, m, x_int, d, minrₑ = minrₑ_int, maxrₑ = maxrₑ_int, verbose = true)
 flux_int = K_int * flux_int
 
-# g grid to do flux integration for narrow line
-gs_narr = range(0.7, 1.1, 500)
-_, flux_narr = lineprofile(gs_narr, ε, m, x_narr, d, minrₑ = minrₑ_narr, maxrₑ = maxrₑ_narr, verbose = true)
+# do flux integration for narrow line
+_, flux_narr = lineprofile(gs, ε, m, x_narr, d, minrₑ = minrₑ_narr, maxrₑ = maxrₑ_narr, verbose = true)
 flux_narr = K_narr * flux_narr
 
 # plot flux as a function of energy for broad line
-plot(gs_broad, flux_broad, xlabel = "Energy", ylabel = "Flux", xrange = (0.75, 1.05), label = "Broad line")
+plot(gs, flux_broad, xlabel = "Energy", ylabel = "Flux", xrange = (0.75, 1.05), label = "Broad line")
 
 # plot flux as a function of energy for intermediate line
-plot!(gs_int, flux_int, xlabel = "Energy", ylabel = "Flux", label = "Intermediate line")
+plot!(gs, flux_int, xlabel = "Energy", ylabel = "Flux", label = "Intermediate line")
 
 # plot flux as a function of energy for narrow line
-plot!(gs_narr, flux_narr, xlabel = "Energy", ylabel = "Flux", label = "Narrow line")
+plot!(gs, flux_narr, xlabel = "Energy", ylabel = "Flux", label = "Narrow line")
+
+# plot total line profile
+plot!(gs, flux_broad + flux_int + flux_narr, xlabel = "Energy", ylabel = "Flux", label = "Total line profile")
